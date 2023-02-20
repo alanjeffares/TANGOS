fe19d830789da22a2aba96ac89834a71af35941b
Alan Jeffares <alanjeffares@Alans-MacBook-Pro-2.local>
2023-02-20 10:26:32 +0000

    fix data paths

--- src/load_data.py
+++ src/load_data.py
@@ -7,48 +7,19 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn import datasets
+import json
 
 
-DEVICE = 'cuda:0'
+def get_path():
+    """Get path to data dir"""
+    results_dir = 'src/data_config.json'
+    with open(results_dir) as f:
+        results = json.load(f)
+    return results['path_to_data']
 
-
-def get_path(device):
-    if re.match("cuda:[0-9]", device):
-        return '/mnt/data/shared/latent-sniper/'
-    elif device == 'cpu':
-        return os.path.dirname(__file__) + '/data/'
-    else:
-        raise ValueError(f'Device {device} not matched to known devices')
-
-
-def load_boston(seed=0, train_prop=0.8, batch_size=64):
-    boston = datasets.load_boston()
-    X, y = boston.data, boston.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_prop, random_state=seed)
-
-    X_scaler = StandardScaler()
-    X_train = X_scaler.fit_transform(X_train)
-    X_test = X_scaler.transform(X_test)
-    y_scaler = StandardScaler()
-    y_train = y_scaler.fit_transform(y_train.reshape(-1,1)).reshape(-1)
-    y_test = y_scaler.transform(y_test.reshape(-1,1)).reshape(-1)
-
-    train_dataset = TensorDataset(torch.Tensor(X_train), torch.Tensor(y_train))
-    test_dataset = TensorDataset(torch.Tensor(X_test), torch.Tensor(y_test))
-
-    loaders = {
-        'train': train_dataset,
-
-        'test': DataLoader(test_dataset,
-                           batch_size=batch_size,
-                           shuffle=False,
-                           num_workers=1)
-    }
-    return loaders
 
 def load_wine(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'winequality-red.csv')
+    data = pd.read_csv(get_path() + 'winequality-red.csv')
     data = data[:1000]
 
     X = data.drop('quality', axis=1)
@@ -77,7 +48,7 @@ def load_wine(seed, train_prop=0.8, batch_size=64):
     return loaders
 
 def load_facebook(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'dataset_facebook.csv', sep=';')
+    data = pd.read_csv(get_path() + 'dataset_facebook.csv', sep=';')
     data.dropna(inplace=True)  # drop missing values
     one_hot = pd.get_dummies(data['Type']) # onehotencode categorical column
     data = data.drop('Type', axis=1)
@@ -110,7 +81,7 @@ def load_facebook(seed, train_prop=0.8, batch_size=64):
 
 
 def load_bioconcentration(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'Grisoni_et_al_2016_EnvInt88.csv', sep=',')
+    data = pd.read_csv(get_path() + 'Grisoni_et_al_2016_EnvInt88.csv', sep=',')
 
     X = data[['nHM', 'piPC09', 'PCD', 'X2Av', 'MLOGP', 'ON1V', 'N-072', 'B02[C-N]', 'F04[C-O]']]
     for var in ['nHM', 'N-072', 'B02[C-N]', 'F04[C-O]']:
@@ -144,7 +115,7 @@ def load_bioconcentration(seed, train_prop=0.8, batch_size=64):
     return loaders
 
 def load_student(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'student-por.csv', sep=';')
+    data = pd.read_csv(get_path() + 'student-por.csv', sep=';')
 
     X = data.drop(['G1', 'G2', 'G3'], axis = 1)
     for var in ['school', 'sex', 'address', 'famsize', 'Pstatus', 'Mjob', 'Fjob',
@@ -181,7 +152,7 @@ def load_student(seed, train_prop=0.8, batch_size=64):
 
 
 def load_abalone(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'abalone.data', sep=',', header=None)
+    data = pd.read_csv(get_path() + 'abalone.data', sep=',', header=None)
     data = data[:1000]
     one_hot = pd.get_dummies(data[0], drop_first=True) # onehotencode categorical column
     data = data.drop(0, axis=1)
@@ -216,7 +187,7 @@ def load_abalone(seed, train_prop=0.8, batch_size=64):
 
 
 def load_skillcraft(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'SkillCraft1_Dataset.csv', sep=',')
+    data = pd.read_csv(get_path() + 'SkillCraft1_Dataset.csv', sep=',')
     data = data.replace('?', np.NaN)
     data = data.dropna()
     data = data[:1000]
@@ -249,7 +220,7 @@ def load_skillcraft(seed, train_prop=0.8, batch_size=64):
 
 
 def load_weather(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'Bias_correction_ucl.csv', sep=',')
+    data = pd.read_csv(get_path() + 'Bias_correction_ucl.csv', sep=',')
     data = data.dropna()
     data = data[:1000]
     one_hot = pd.get_dummies(data['station'], drop_first=True)  # onehotencode categorical column
@@ -284,7 +255,7 @@ def load_weather(seed, train_prop=0.8, batch_size=64):
 
 
 def load_forest(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'forestfires.csv', sep=',')
+    data = pd.read_csv(get_path() + 'forestfires.csv', sep=',')
     X = data.drop('area', axis=1)
     for var in ['X', 'Y', 'month', 'day']:
         one_hot = pd.get_dummies(X[var], prefix=var, drop_first=True)  # onehotencode categorical column
@@ -317,7 +288,7 @@ def load_forest(seed, train_prop=0.8, batch_size=64):
     return loaders
 
 def load_protein(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'CASP.csv', sep=',')
+    data = pd.read_csv(get_path() + 'CASP.csv', sep=',')
     data = data[:1000]
     X = data.drop('RMSD', axis=1)
     y = data['RMSD']
@@ -348,7 +319,7 @@ def load_protein(seed, train_prop=0.8, batch_size=64):
 
 
 def load_heart(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'heart.dat', sep=' ', header=None)
+    data = pd.read_csv(get_path() + 'heart.dat', sep=' ', header=None)
     X = data.drop(13, axis=1)
     y = data[13] - 1
     data[9] = np.log(data[9] + 1)
@@ -380,7 +351,7 @@ def load_heart(seed, train_prop=0.8, batch_size=64):
 
 
 def load_breast(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'breast-cancer-wisconsin.data', header=None)
+    data = pd.read_csv(get_path() + 'breast-cancer-wisconsin.data', header=None)
     X = data.drop([0, 10], axis=1)
     X[6].replace('?', np.nan, inplace=True)
     X[6].fillna((X[6].median()), inplace=True)
@@ -408,7 +379,7 @@ def load_breast(seed, train_prop=0.8, batch_size=64):
     return loaders
 
 def load_cervical(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'risk_factors_cervical_cancer.csv')
+    data = pd.read_csv(get_path() + 'risk_factors_cervical_cancer.csv')
     X = data[['Age', 'Number of sexual partners', 'First sexual intercourse',
           'Num of pregnancies', 'Smokes', 'Smokes (years)', 'Smokes (packs/year)',
           'Hormonal Contraceptives', 'Hormonal Contraceptives (years)', 'IUD',
@@ -458,7 +429,7 @@ def load_cervical(seed, train_prop=0.8, batch_size=64):
     return loaders
 
 def load_credit(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + "crx.data", header = None)
+    data = pd.read_csv(get_path() + "crx.data", header = None)
     data = data[data[13] != '?']
     data[13] = np.log(data[13].astype(int) + 1)
     data[14] = np.log(data[14] + 1)
@@ -497,7 +468,7 @@ def load_credit(seed, train_prop=0.8, batch_size=64):
 
 
 def load_hcv(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + "hcvdat0.csv", index_col=0)
+    data = pd.read_csv(get_path() + "hcvdat0.csv", index_col=0)
     y = data['Category'].apply(lambda x: int(x[0]))
     X = data.drop('Category', axis=1)
     one_hot = pd.get_dummies(X['Sex'], prefix='Sex', drop_first=True)  # onehotencode categorical column
@@ -528,7 +499,7 @@ def load_hcv(seed, train_prop=0.8, batch_size=64):
 
 
 def load_tumor(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'primary-tumor.data', header=None)
+    data = pd.read_csv(get_path() + 'primary-tumor.data', header=None)
     y = data[0] - 1
     X = data.drop(0, axis=1)
     for var in X.columns:
@@ -558,8 +529,8 @@ def load_tumor(seed, train_prop=0.8, batch_size=64):
     return loaders
 
 def load_soybean(seed, train_prop=0.8, batch_size=64):
-    data0 = pd.read_csv(get_path(DEVICE) + 'soybean-large.data', header=None)
-    data1 = pd.read_csv(get_path(DEVICE) + 'soybean-large.test', header=None)
+    data0 = pd.read_csv(get_path() + 'soybean-large.data', header=None)
+    data1 = pd.read_csv(get_path() + 'soybean-large.test', header=None)
     data = pd.concat([data0, data1], axis=0)
     data.reset_index(inplace=True)
     y = data[0].rank(method='dense', ascending=False).astype(int) - 1
@@ -591,7 +562,7 @@ def load_soybean(seed, train_prop=0.8, batch_size=64):
     return loaders
 
 def load_australian(seed, train_prop=0.8, batch_size=64):
-    data = pd.read_csv(get_path(DEVICE) + 'australian.dat', sep=' ', header=None)
+    data = pd.read_csv(get_path() + 'australian.dat', sep=' ', header=None)
     data[2] = np.log(data[2] + 1)
     data[6] = np.log(data[6] + 1)
     data[12] = np.log(data[12] + 1)
@@ -624,7 +595,7 @@ def load_australian(seed, train_prop=0.8, batch_size=64):
     return loaders
 
 def load_entrance(seed, train_prop=0.8, batch_size=64):
-    data_arff = arff.load(open(get_path(DEVICE) + 'CEE_DATA.arff'))
+    data_arff = arff.load(open(get_path() + 'CEE_DATA.arff'))
     data = pd.DataFrame(data_arff['data'])
     y = data[0].rank(method='dense', ascending=False).astype(int) - 1
     X = data.drop(0, axis=1)
@@ -654,7 +625,7 @@ def load_entrance(seed, train_prop=0.8, batch_size=64):
     return loaders
 
 def load_thoracic(seed, train_prop=0.8, batch_size=64):
-    data_arff = arff.load(open(get_path(DEVICE) + 'ThoraricSurgery.arff'))
+    data_arff = arff.load(open(get_path() + 'ThoraricSurgery.arff'))
     data = pd.DataFrame(data_arff['data'])
     y = data[16]
     y = y.replace({'T':1, 'F':0})
